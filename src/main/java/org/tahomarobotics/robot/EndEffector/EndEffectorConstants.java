e60e7fb3df5dfa4b670ecff4616b4fa1b67625ab
RyzenEvolutionX3 <piyushkancharapu@outlook.com>
2025-10-12 16:59:05 -0700

    Majority of the changes requested have been made

--- src/main/java/org/tahomarobotics/robot/EndEffector/EndEffectorConstants.java
+++ src/main/java/org/tahomarobotics/robot/EndEffector/EndEffectorConstants.java
@@ -4,8 +4,7 @@ public class EndEffectorConstants {
 
 
 
-    //Physical Constants
-    //Source: Lines 291-294 NEW_SUBSYSTEM_GUIDE.md
+
     public static final double GEAR_RATIO = 10.0; // this is a place holder not the acutual gear ratio
 
 
