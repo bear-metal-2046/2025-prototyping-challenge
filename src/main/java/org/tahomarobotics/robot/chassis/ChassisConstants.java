17932d0f606ba65dbba938a72f2a3ba053ced9ea
E <eli.tribhuwan12@gmail.com>
2025-10-14 16:45:13 -0700

    added drive, steer, and encoder

--- src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
@@ -1,5 +1,73 @@
 package org.tahomarobotics.robot.chassis;
 
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+
+
 public class ChassisConstants {
 
+
+    // Current Limits
+
+    private static final double DRIVE_SUPPLY_CURRENT_LIMIT = 80;
+    private static final double DRIVE_STATOR_CURRENT_LIMIT = 160;
+
+
+
+    public static final double steerRatio = (1 /(150.0 /7)); /*Gotten from the Swerve Drive Specialists MK4i swerve module */
+
+
+
+    public static final CurrentLimitsConfigs teleopConfiguration =
+
+            new CurrentLimitsConfigs()
+                    .withStatorCurrentLimitEnable(false)
+                    .withSupplyCurrentLimitEnable(true)
+                    .withStatorCurrentLimit(DRIVE_STATOR_CURRENT_LIMIT)
+                    .withSupplyCurrentLimit(DRIVE_SUPPLY_CURRENT_LIMIT);
+
+
+    public static TalonFXConfiguration createDriveMotorConfig() {
+
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        return new TalonFXConfiguration()
+
+                .withMotorOutput(
+                        new MotorOutputConfigs()
+                                .withNeutralMode(NeutralModeValue.Brake)
+                                .withInverted(InvertedValue.Clockwise_Positive)
+                ).withCurrentLimits(
+                        teleopConfiguration
+                );
+    }
+
+
+    public static TalonFXConfiguration createSteerMotorConfig(int encoderId, double steerRatio) {
+        return new TalonFXConfiguration()
+                .withMotorOutput(
+                        new MotorOutputConfigs()
+                                .withNeutralMode(NeutralModeValue.Brake)
+                                .withInverted(InvertedValue.Clockwise_Positive)
+                ).withFeedback(new FeedbackConfigs()
+                        .withFeedbackRemoteSensorID(encoderId)
+                        .withRotorToSensorRatio(steerRatio)
+                );
+    }
+
+
+
+
+    public static CANcoderConfiguration createEncoderConfig() {
+        return new CANcoderConfiguration()
+                .withMagnetSensor(
+                        new MagnetSensorConfigs()
+                                .withAbsoluteSensorDiscontinuityPoint(1)
+                                .withSensorDirection(SensorDirectionValue.CounterClockwise_Positive)
+                );
+    }
 }
+
+
--- src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
+++ src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
@@ -5,9 +5,11 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.tahomarobotics.robot.RobotMap;
+import org.tahomarobotics.robot.util.RobustConfigurator;
 import org.tinylog.Logger;
 
 import static org.tahomarobotics.robot.RobotMap.CANBUS_NAME;
+import static org.tahomarobotics.robot.chassis.ChassisConstants.*;
 
 
 class SwerveModule extends SubsystemBase {
@@ -30,6 +32,13 @@ class SwerveModule extends SubsystemBase {
         driveMotor = new TalonFX(descriptor.driveId(), CANBUS_NAME);
         steerMotor = new TalonFX(descriptor.steerId(), CANBUS_NAME);
         steerEncoder = new CANcoder(descriptor.cancoderId(), CANBUS_NAME);
+
+
+        RobustConfigurator.tryConfigureTalonFX(" Drive Motor", driveMotor, createDriveMotorConfig());
+        RobustConfigurator.tryConfigureTalonFX(" Steer Motor", steerMotor, createSteerMotorConfig(descriptor.cancoderId(), ChassisConstants.steerRatio));
+        RobustConfigurator.tryConfigureCANcoder(" Encoder ", steerEncoder, createEncoderConfig());
+
+
 
         Logger.info("Creating an instance of SwerveModule: Drive Motor, Steer Motor, and Encoder ");
 
