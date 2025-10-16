3611fc267ac9062897119abb9fc80f48d81c7a6c
E <eli.tribhuwan12@gmail.com>
2025-10-16 16:33:35 -0700

    added logs for steer/drive,

--- src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
+++ src/main/java/org/tahomarobotics/robot/chassis/SwerveModule.java
@@ -1,12 +1,18 @@
 package org.tahomarobotics.robot.chassis;
 
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.units.measure.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.tahomarobotics.robot.RobotMap;
 import org.tahomarobotics.robot.util.RobustConfigurator;
-import org.tinylog.Logger;
+import org.littletonrobotics.junction.Logger;
+import org.tahomarobotics.robot.util.signals.LoggedStatusSignal;
+import edu.wpi.first.units.measure.AngularVelocity;
+
+import java.util.function.BooleanSupplier;
 
 import static org.tahomarobotics.robot.RobotMap.CANBUS_NAME;
 import static org.tahomarobotics.robot.chassis.ChassisConstants.*;
@@ -14,20 +20,33 @@ import static org.tahomarobotics.robot.chassis.ChassisConstants.*;
 
 class SwerveModule extends SubsystemBase {
 
+    //Identifiers
+    private final String name;
+
     //Hardware
     private final TalonFX driveMotor;
     private final TalonFX steerMotor;
     private final CANcoder steerEncoder;
 
-    //Identifiers
-    private final String name;
-
-
+    // state
     private SwerveModuleState targetState = new SwerveModuleState();
+
+    //Status Signals
+    private final StatusSignal<Angle> steerPosition;
+    private final StatusSignal<AngularVelocity> steerVelocity;
+    private final StatusSignal<Angle> drivePosition;
+    private final StatusSignal<Angle> driveRotorPosition;
+    private final StatusSignal<AngularVelocity> driveVelocity;
+    private final StatusSignal<Current> driveCurrent, steerCurrent;
+    private final StatusSignal<Voltage> driveVoltage, steerVoltage;
+
+
+    private final LoggedStatusSignal[] statusSignals;
 
     //Constructors
     public SwerveModule(RobotMap.moduleId descriptor) {
         name = descriptor.moduleName();
+
 
         driveMotor = new TalonFX(descriptor.driveId(), CANBUS_NAME);
         steerMotor = new TalonFX(descriptor.steerId(), CANBUS_NAME);
@@ -35,12 +54,49 @@ class SwerveModule extends SubsystemBase {
 
 
         RobustConfigurator.tryConfigureTalonFX(" Drive Motor", driveMotor, createDriveMotorConfig());
-        RobustConfigurator.tryConfigureTalonFX(" Steer Motor", steerMotor, createSteerMotorConfig(descriptor.cancoderId(), ChassisConstants.steerRatio));
+        RobustConfigurator.tryConfigureTalonFX(" Steer Motor", steerMotor, createSteerMotorConfig(descriptor.cancoderId(), steerRatio));
         RobustConfigurator.tryConfigureCANcoder(" Encoder ", steerEncoder, createEncoderConfig());
 
 
+        statusSignals = new LoggedStatusSignal[]{
+                new LoggedStatusSignal("Steer Position", steerMotor.getPosition()),
+                new LoggedStatusSignal("Steer Velocity", steerMotor.getVelocity()),
+                new LoggedStatusSignal("Steer Current", steerMotor.getStatorCurrent()),
+                new LoggedStatusSignal("Steer Voltage", steerMotor.getMotorVoltage()),
 
-        Logger.info("Creating an instance of SwerveModule: Drive Motor, Steer Motor, and Encoder ");
+                new LoggedStatusSignal("Drive Position", driveMotor.getPosition()),
+                new LoggedStatusSignal("Drive Rotor Position", driveMotor.getRotorPosition()),
+                new LoggedStatusSignal("Drive Velocity", driveMotor.getVelocity()),
+                new LoggedStatusSignal("Drive Current", driveMotor.getStatorCurrent()),
+                new LoggedStatusSignal("Drive Voltage", driveMotor.getMotorVoltage()),
+        };
+
+        steerPosition = steerMotor.getPosition();
+        steerVelocity = steerMotor.getVelocity();
+        steerCurrent = steerMotor.getStatorCurrent();
+        steerVoltage = steerMotor.getMotorVoltage();
+
+
+        drivePosition = driveMotor.getPosition();
+        driveRotorPosition = driveMotor.getRotorPosition();
+        driveVelocity = driveMotor.getVelocity();
+        driveCurrent = driveMotor.getStatorCurrent();
+        driveVoltage = driveMotor.getMotorVoltage();
+
+        Logger.recordOutput("steer/position", (BooleanSupplier) steerPosition);
+        Logger.recordOutput("steer/velocity", (BooleanSupplier) steerVelocity);
+        Logger.recordOutput("steer/current", (BooleanSupplier) steerCurrent);
+        Logger.recordOutput("steer/voltage", (BooleanSupplier) steerVoltage);
+
+        Logger.recordOutput("drive/position", (BooleanSupplier) drivePosition);
+        Logger.recordOutput("drive/rotorPosition", (BooleanSupplier) driveRotorPosition);
+        Logger.recordOutput("drive/velocity", (BooleanSupplier) driveVelocity);
+        Logger.recordOutput("drive/current", (BooleanSupplier) driveCurrent);
+        Logger.recordOutput("drive/voltage", (BooleanSupplier) driveVoltage);
+
+
+
+
 
     }
 }
