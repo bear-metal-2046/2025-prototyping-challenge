6c1f8920df4e9f0386ef3d0b65ce342d68d8c9fc
pkancharapu436 <piyushkancharapu@outlook.com>
2025-11-04 16:33:21 -0800

    Added the TalonFZ CONFIG and only used known values for the config to have a working Talon FX configuration. I then I used Robust Configurator to set the motor to the set config. This is ready for Review and it passes all tests.

--- src/main/java/org/tahomarobotics/robot/endeffector/EndEffectorConstants.java
+++ src/main/java/org/tahomarobotics/robot/endeffector/EndEffectorConstants.java
@@ -1,11 +1,33 @@
 package org.tahomarobotics.robot.endeffector;
 
+import com.ctre.phoenix6.configs.*;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import org.tahomarobotics.robot.RobotMap;
+
 public class EndEffectorConstants {
 
 
 
 
-    public static final double GEAR_RATIO = 10.0; // this is a place holder not the acutual gear ratio
+    public static final double GEAR_RATIO = 10.0; // this is a placeholder not the acutual gear ratio
+    static final TalonFXConfiguration endEffectorMotorConfig = new TalonFXConfiguration()
+            .withSlot0(
+                    new Slot0Configs()
+                            .withGravityType(GravityTypeValue.Arm_Cosine)
+            )
+            .withMotorOutput(new MotorOutputConfigs()
+                    .withNeutralMode(NeutralModeValue.Brake)
+                    .withInverted(InvertedValue.CounterClockwise_Positive))
+            .withMotionMagic(new MotionMagicConfigs()
+                    .withMotionMagicCruiseVelocity(40)
+                    .withMotionMagicAcceleration(100)
+                    .withMotionMagicJerk(5000))
+            .withFeedback(new FeedbackConfigs().withSensorToMechanismRatio(1 / GEAR_RATIO))
+            .withAudio(new AudioConfigs().withBeepOnBoot(true).withBeepOnConfig(true));
+
 
 
 }
--- src/main/java/org/tahomarobotics/robot/endeffector/EndEffectorSubsystem.java
+++ src/main/java/org/tahomarobotics/robot/endeffector/EndEffectorSubsystem.java
@@ -3,14 +3,18 @@ package org.tahomarobotics.robot.endeffector;
 import com.ctre.phoenix6.hardware.TalonFX;
 import org.tahomarobotics.robot.RobotMap;
 import org.tahomarobotics.robot.util.AbstractSubsystem;
+import org.tahomarobotics.robot.util.RobustConfigurator;
 import org.tinylog.Logger;
 
-    public class EndEffectorSubsystem extends AbstractSubsystem {
+import static org.tahomarobotics.robot.endeffector.EndEffectorConstants.endEffectorMotorConfig;
+
+public class EndEffectorSubsystem extends AbstractSubsystem {
 
         private final TalonFX endEffectorMotor;
 
         public EndEffectorSubsystem() {
             endEffectorMotor = new TalonFX(RobotMap.ENDEFFECTOR_MOTOR);
+            RobustConfigurator.tryConfigureTalonFX("End Effector Motor", endEffectorMotor, endEffectorMotorConfig);
         Logger.info("End Effector Motor has been created");
     }
 
