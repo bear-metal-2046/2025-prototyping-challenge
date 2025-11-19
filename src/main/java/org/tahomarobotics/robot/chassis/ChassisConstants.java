8bc04340a6d49afeae990b0cf4a2be643826b643
pranavp2010 <pranavp232010@gmail.com>
2025-11-19 15:50:34 -0800

    Uncommented known swerve module constants in the constant factory and filled in parameters with created private static final constants

--- src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
@@ -13,40 +13,56 @@ import org.tahomarobotics.robot.RobotMap.*;
 
 import java.util.Map;
 
+import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 import static org.tahomarobotics.robot.RobotMap.*;
 
 public class ChassisConstants {
+
+    public static final double DRIVE_GEAR_RATIO = (54d / 14d) * (25d / 32d) * (30d / 15d);
+    public static final double STEER_GEAR_RATIO = (49d / 7d) * (41d / 11d);
+    public static final double COUPLING_GEAR_RATIO = 54d / 14d;
+    public static final Distance WHEEL_RADIUS = Inches.of(2d);
+
+    private static final SwerveModuleConstants.ClosedLoopOutputType STEER_CLOSED_LOOP_OUTPUT = SwerveModuleConstants.ClosedLoopOutputType.Voltage;
+    private static final SwerveModuleConstants.ClosedLoopOutputType DRIVE_CLOSED_LOOP_OUTPUT = SwerveModuleConstants.ClosedLoopOutputType.Voprivate
+    private static final SwerveModuleConstants.DriveMotorArrangement DRIVE_MOTOR_TYPE = SwerveModuleConstants.DriveMotorArrangement.TalonFX_Integrated;
+    private static final SwerveModuleConstants.SteerMotorArrangement STEER_MOTOR_TYPE = SwerveModuleConstants.SteerMotorArrangement.TalonFX_Integprivate
+    private static final SwerveModuleConstants.SteerFeedbackType STEER_FEEDBACK_TYPE = SwerveModuleConstants.SteerFeedbackType.FusedCANprivate
+    private static final TalonFXConfiguration DRIVE_MOTOR_CONFIG = new TalonFXConfiguration();
+    private static final TalonFXConfiguration STEER_MOTOR_CONFIG = new TalonFXConfiguration();
+    private static final CANcoderConfiguration CANCODER_CONFIG = new CANcoderConfiguration();
+
     public static final SwerveDrivetrainConstants DRIVETRAIN_CONSTANTS =
             new SwerveDrivetrainConstants()
                     .withCANBusName(CANBUS_NAME)
                     .withPigeon2Id(PIGEON);
 
     private static final SwerveModuleConstantsFactory<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> MODULE_CONSTANTS_FACTORY =
-            new SwerveModuleConstantsFactory<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration>();
-//                    .withDriveMotorGearRatio(kDriveGearRatio)
-//                    .withSteerMotorGearRatio(kSteerGearRatio)
-//                    .withCouplingGearRatio(kCoupleRatio)
-//                    .withWheelRadius(kWheelRadius)
+            new SwerveModuleConstantsFactory<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration>()
+                  .withDriveMotorGearRatio(DRIVE_GEAR_RATIO)
+                  .withSteerMotorGearRatio(STEER_GEAR_RATIO)
+                  .withCouplingGearRatio(COUPLING_GEAR_RATIO)
+                  .withWheelRadius(WHEEL_RADIUS)
 //                    .withSteerMotorGains(steerGains)
 //                    .withDriveMotorGains(driveGains)
-//                    .withSteerMotorClosedLoopOutput(kSteerClosedLoopOutput)
-//                    .withDriveMotorClosedLoopOutput(kDriveClosedLoopOutput)
+                  .withSteerMotorClosedLoopOutput(STEER_CLOSED_LOOP_OUTPUT)
+                  .withDriveMotorClosedLoopOutput(DRIVE_CLOSED_LOOP_OUTPUT)
 //                    .withSlipCurrent(kSlipCurrent)
 //                    .withSpeedAt12Volts(kSpeedAt12Volts)
-//                    .withDriveMotorType(kDriveMotorType)
-//                    .withSteerMotorType(kSteerMotorType)
-//                    .withFeedbackSource(kSteerFeedbackType)
-//                    .withDriveMotorInitialConfigs(driveInitialConfigs)
-//                    .withSteerMotorInitialConfigs(steerInitialConfigs)
-//                    .withEncoderInitialConfigs(encoderInitialConfigs)
+                  .withDriveMotorType(DRIVE_MOTOR_TYPE)
+                  .withSteerMotorType(STEER_MOTOR_TYPE)
+                    .withFeedbackSource(STEER_FEEDBACK_TYPE)
+                  .withDriveMotorInitialConfigs(DRIVE_MOTOR_CONFIG)
+                  .withSteerMotorInitialConfigs(STEER_MOTOR_CONFIG)
+                  .withEncoderInitialConfigs(CANCODER_CONFIG);
 //                    .withSteerInertia(kSteerInertia)
 //                    .withDriveInertia(kDriveInertia)
 //                    .withSteerFrictionVoltage(kSteerFrictionVoltage)
 //                    .withDriveFrictionVoltage(kDriveFrictionVoltage);
 
-    public static final Distance HALF_TRACK_WIDTH = Meters.of(20.75 / 2.0);
-    public static final Distance HALF_WHEELBASE = Meters.of(20.75 / 2.0);
+    public static final Distance HALF_TRACK_WIDTH = Meters.of(20.75d / 2d);
+    public static final Distance HALF_WHEELBASE = Meters.of(20.75d / 2d);
 
     private static final Map<ModuleId, Distance> DISTANCE_X = Map.of(
             FRONT_LEFT_MODULE, HALF_WHEELBASE,
