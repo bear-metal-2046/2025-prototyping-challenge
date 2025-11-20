e7e6d01576df55e772242b3c22ec2c4de67df0f6
Jasdonza <Jasdonza@gmail.com>
2025-11-20 15:15:41 -0800

    Removed elevator button controls from OI, fixed the moveToPosition comment, and fixed the control request, and fixed the name for motor Temperature.

--- src/main/java/org/tahomarobotics/robot/elevator/ElevatorSubsystem.java
+++ src/main/java/org/tahomarobotics/robot/elevator/ElevatorSubsystem.java
@@ -92,8 +92,8 @@ class ElevatorSubsystem extends AbstractSubsystem {
         Logger.recordOutput("Elevator/Currents/Left Motor Supply Current", leftMotor.getSupplyCurrent().getValue());
         Logger.recordOutput("Elevator/Currents/Right Motor Stator Current", rightMotor.getStatorCurrent().getValue());
         Logger.recordOutput("Elevator/Currents/Right Motor Supply Current", rightMotor.getSupplyCurrent().getValue());
-        Logger.recordOutput("Elevator/Temperature/Right Motor Supply Current", rightMotor.getDeviceTemp().getValue());
-        Logger.recordOutput("Elevator/Temperature/Left Motor Supply Current", leftMotor.getDeviceTemp().getValue());
+        Logger.recordOutput("Elevator/Temperature/Right Motor Temperature", rightMotor.getDeviceTemp().getValue());
+        Logger.recordOutput("Elevator/Temperature/Left Motor Temperature", leftMotor.getDeviceTemp().getValue());
 
         //Logging Raw Sensor Feedback
         Logger.recordOutput("Elevator/Sensor Feedback/Left Motor", leftMotor.getPosition().getValue());
