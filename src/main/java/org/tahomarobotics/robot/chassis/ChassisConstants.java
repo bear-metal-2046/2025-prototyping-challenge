0ca7c5eb1623b77cd5058d39de41ae870f654aee
pranavp2010 <pranavp232010@gmail.com>
2025-11-20 17:02:48 -0800

    created a method for controller input desensitization in chassis, created constants for the offset of the modules to the center of the robot, the radius of the robot, max angular and linear velocities at 12 volts, and created controller deadbands and sensitivities, and then updated the module degree offset

--- src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisConstants.java
@@ -1,23 +1,24 @@
 package org.tahomarobotics.robot.chassis;
 
 import com.ctre.phoenix6.configs.*;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
 import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstantsFactory;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Distance;
-import org.tahomarobotics.robot.RobotMap.*;
+import edu.wpi.first.units.measure.LinearVelocity;
 
 import java.util.Map;
 
-import static edu.wpi.first.units.Units.Inches;
-import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.*;
 import static org.tahomarobotics.robot.RobotMap.*;
 
 public class ChassisConstants {
+    public static final double CONTROLLER_ROTATIONAL_SENSITIVITY = 2d;
+    public static final double CONTROLLER_TRANSLATIONAL_SENSITIVITY = 1.3d;
+    public static final double CONTROLLER_DEADBAND = 0.09d;
 
     public static final double DRIVE_GEAR_RATIO = (54d / 14d) * (25d / 32d) * (30d / 15d);
     public static final double STEER_GEAR_RATIO = (49d / 7d) * (41d / 11d);
@@ -26,9 +27,19 @@ public class ChassisConstants {
 
     private static final SwerveModuleConstants.ClosedLoopOutputType STEER_CLOSED_LOOP_OUTPUT = SwerveModuleConstants.ClosedLoopOutputType.Voltage;
     private static final SwerveModuleConstants.ClosedLoopOutputType DRIVE_CLOSED_LOOP_OUTPUT = SwerveModuleConstants.ClosedLoopOutputType.Voltage;
+
+    public static final Distance HALF_TRACK_WIDTH = Meters.of(20.75d / 2d);
+    public static final Distance HALF_WHEELBASE = Meters.of(20.75d / 2d);
+
+    public static final Distance ROBOT_RADIUS = Meters.of(Math.hypot(HALF_TRACK_WIDTH.in(Meters), HALF_WHEELBASE.in(Meters)));
+
+    public static final LinearVelocity MAX_LINEAR_VELOCITY = MetersPerSecond.of(DCMotor.getKrakenX60(1).withReduction(DRIVE_GEAR_RATIO).getSpeed(0, 12.0));
+    public static final AngularVelocity MAX_ANGULAR_VELOCITY = RadiansPerSecond.of(MAX_LINEAR_VELOCITY.in(MetersPerSecond) / ROBOT_RADIUS.in(Meters));
+
     private static final SwerveModuleConstants.DriveMotorArrangement DRIVE_MOTOR_TYPE = SwerveModuleConstants.DriveMotorArrangement.TalonFX_Integrated;
     private static final SwerveModuleConstants.SteerMotorArrangement STEER_MOTOR_TYPE = SwerveModuleConstants.SteerMotorArrangement.TalonFX_Integrated;
     private static final SwerveModuleConstants.SteerFeedbackType STEER_FEEDBACK_TYPE = SwerveModuleConstants.SteerFeedbackType.FusedCANcoder;
+
     private static final TalonFXConfiguration DRIVE_MOTOR_CONFIG = new TalonFXConfiguration();
     private static final TalonFXConfiguration STEER_MOTOR_CONFIG = new TalonFXConfiguration();
     private static final CANcoderConfiguration CANCODER_CONFIG = new CANcoderConfiguration();
@@ -49,7 +60,7 @@ public class ChassisConstants {
                   .withSteerMotorClosedLoopOutput(STEER_CLOSED_LOOP_OUTPUT)
                   .withDriveMotorClosedLoopOutput(DRIVE_CLOSED_LOOP_OUTPUT)
 //                    .withSlipCurrent(kSlipCurrent)
-//                    .withSpeedAt12Volts(kSpeedAt12Volts)
+                  .withSpeedAt12Volts(MAX_LINEAR_VELOCITY)
                   .withDriveMotorType(DRIVE_MOTOR_TYPE)
                   .withSteerMotorType(STEER_MOTOR_TYPE)
                     .withFeedbackSource(STEER_FEEDBACK_TYPE)
@@ -60,9 +71,6 @@ public class ChassisConstants {
 //                    .withDriveInertia(kDriveInertia)
 //                    .withSteerFrictionVoltage(kSteerFrictionVoltage)
 //                    .withDriveFrictionVoltage(kDriveFrictionVoltage);
-
-    public static final Distance HALF_TRACK_WIDTH = Meters.of(20.75d / 2d);
-    public static final Distance HALF_WHEELBASE = Meters.of(20.75d / 2d);
 
     private static final Map<ModuleId, Distance> DISTANCE_X = Map.of(
             FRONT_LEFT_MODULE, HALF_WHEELBASE,
