54c294d7bfb5c4971f1df80904d7ea3bc9586a64
Jasdonza <Jasdonza@gmail.com>
2025-11-21 16:06:40 -0800

    reset Elevator

--- src/main/java/org/tahomarobotics/robot/elevator/Elevator.java
+++ src/main/java/org/tahomarobotics/robot/elevator/Elevator.java
@@ -12,19 +12,41 @@ import org.tinylog.Supplier;
 import static edu.wpi.first.units.Units.*;
 import static org.tahomarobotics.robot.elevator.ElevatorConstants.ELEVATOR_ZERO_VOLTAGE;
 
-public class Elevator implements AutoCloseable { private final ElevatorSubsystem elevator;
+public class Elevator implements AutoCloseable {
+    private final ElevatorSubsystem elevator;
 
 
+    public Elevator() {
+        this(new ElevatorSubsystem());
+    }
 
-                 public Elevator() {this(new ElevatorSubsystem());}public
-          Elevator(ElevatorSubsystem elevatorSubsystem) {this.elevator = elevatorSubsystem;
-        Logger.info("Elevator initialized");}public Command setPosition(Distance position) {return
-     elevator.runOnce(() -> elevator.moveToPosition(position)).withName("Elevator move to Position");}
-    public Command stop() {return elevator. /* */runOnce(() -> elevator.stop()).withName("Elevator Stop");}
-   public Command homing(){return elevator./*    */runOnce(() -> elevator.setElevatorVoltage(ELEVATOR_ZERO_VOLTAGE.
-  in(Volt))).andThen(Commands.waitUntil(()/*  || */-> elevator.getCarriageVelocity().isNear(Rotations.per(Second)
-  .of(0.0), Rotations.per(Second)/* ||  */.of(0.01)))).andThen(Commands.runOnce(() -> elevator
-   .setCarriagePos(0.0))).andThen(Commands./*    */runOnce(() -> Logger.info("Elevator has Homed")));}public
- Command manualJog(Supplier<Voltage> voltage)/* */ {return Commands.run(() ->elevator.setElevatorVoltage
-    (voltage.get().in(Volt))).andThen(Commands.runOnce(() -> Logger.info("Elevator is Jogging")));}
-                   @Override public void close() throws Exception {}}
+    public Elevator(ElevatorSubsystem elevatorSubsystem) {
+        this.elevator = elevatorSubsystem;
+        Logger.info("Elevator initialized");
+    }
+
+    public Command setPosition(Distance position) {
+        return
+                elevator.runOnce(() -> elevator.moveToPosition(position)).withName("Elevator move to Position");
+    }
+
+    public Command stop() {
+        return elevator. /* */runOnce(() -> elevator.stop()).withName("Elevator Stop");
+    }
+
+    public Command homing() {
+        return elevator./*    */runOnce(() -> elevator.setElevatorVoltage(ELEVATOR_ZERO_VOLTAGE.
+                in(Volt))).andThen(Commands.waitUntil(()/*  || */ -> elevator.getCarriageVelocity().isNear(Rotations.per(Second)
+                .of(0.0), Rotations.per(Second)/* ||  */.of(0.01)))).andThen(Commands.runOnce(() -> elevator
+                .setCarriagePos(0.0))).andThen(Commands./*    */runOnce(() -> Logger.info("Elevator has Homed")));
+    }
+
+    public Command manualJog(Supplier<Voltage> voltage)/* */ {
+        return Commands.run(() -> elevator.setElevatorVoltage
+                (voltage.get().in(Volt))).andThen(Commands.runOnce(() -> Logger.info("Elevator is Jogging")));
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
