636a4c8afaf21eb480d92a800e5c3788ecc90c0c
Jasdonza <Jasdonza@gmail.com>
2025-12-11 15:54:41 -0800

    Now running and logging the getEstimatedRobotPose, and fixed the limelight camera name

--- src/main/java/org/tahomarobotics/robot/vision/VisionSubsystem.java
+++ src/main/java/org/tahomarobotics/robot/vision/VisionSubsystem.java
@@ -1,11 +1,19 @@
 package org.tahomarobotics.robot.vision;
 
-public class VisionSubsystem {
+import org.littletonrobotics.junction.Logger;
+import org.tahomarobotics.robot.util.AbstractSubsystem;
+
+public class VisionSubsystem extends AbstractSubsystem {
 
     private final Limelight limelight;
 
     VisionSubsystem() {
-        this(new Limelight("Limelight Camera"));
+        this(new Limelight("Limelight"));
+    }
+
+    @Override
+    public void subsystemPeriodic() {
+        Logger.recordOutput("Vision/Position", limelight.getEstimatedRobotPose());
     }
 
     private VisionSubsystem(Limelight limelight){
