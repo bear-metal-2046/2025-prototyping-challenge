f6478613fe6cf677abdb316ca670ccdbcc99cab6
Jasdonza <Jasdonza@gmail.com>
2025-12-11 15:38:40 -0800

    Made EstimatedRobotPose and getEstimatedRobotPose

--- src/main/java/org/tahomarobotics/robot/vision/Limelight.java
+++ src/main/java/org/tahomarobotics/robot/vision/Limelight.java
@@ -1,5 +1,10 @@
 package org.tahomarobotics.robot.vision;
 
+
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.numbers.N3;
+import org.tahomarobotics.robot.util.LimelightHelpers;
+
 public class Limelight {
 
     // --- Fields and Constants ---
@@ -14,6 +19,12 @@ public class Limelight {
         this.name = name;
     }
 
+    public EstimatedRobotPose getEstimatedRobotPose() {
+        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(name);
+        return new EstimatedRobotPose(mt1, this);
+    }
+
+    public record EstimatedRobotPose(LimelightHelpers.PoseEstimate poseEstimate, Limelight camera) {}
 
 
 }
