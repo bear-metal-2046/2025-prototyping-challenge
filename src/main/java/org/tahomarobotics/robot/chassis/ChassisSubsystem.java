bca19386b897a3624160e97bb2a9c330a4b6dd3c
Robert Davis <robertdavis.gocougs@gmail.com>
2025-12-17 09:44:22 -0800

    no sim while real

--- src/main/java/org/tahomarobotics/robot/chassis/ChassisSubsystem.java
+++ src/main/java/org/tahomarobotics/robot/chassis/ChassisSubsystem.java
@@ -57,7 +57,7 @@ public class ChassisSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcode
             SwerveModuleConstants<?, ?, ?>... modules) {
         super(driveMotorConstructor, steerMotorConstructor, encoderConstructor, drivetrainConstants, modules);
 
-        simulation = new ChassisSimulation(getPigeon2(), getModules());
+        simulation = Robot.isSimulated() ?  new ChassisSimulation(getPigeon2(), getModules()) : null;
     }
 
     public ChassisSubsystem() {
