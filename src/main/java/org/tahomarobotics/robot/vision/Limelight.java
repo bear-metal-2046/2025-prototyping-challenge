7667a584454cd5924081a2375980b5b26cb98612
Jasdonza <jasdonza@gmail.com>
2026-01-01 18:31:10 -0800

    Limelight now has a setCameraOffset function, and VisionConstants now has a way to set CameraConfiguration. both are not complete as I have run into problems that I do not know how to do

--- src/main/java/org/tahomarobotics/robot/vision/Limelight.java
+++ src/main/java/org/tahomarobotics/robot/vision/Limelight.java
@@ -2,6 +2,7 @@ package org.tahomarobotics.robot.vision;
 
 import java.util.Optional;
 
+import edu.wpi.first.math.geometry.Transform3d;
 import org.tahomarobotics.robot.util.LimelightHelpers;
 
 public class Limelight {
@@ -17,11 +18,24 @@ public class Limelight {
 
     public Optional<EstimatedRobotPose> getEstimatedRobotPose() {
         LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue(name);
+        LimelightHelpers.setCameraPose_RobotSpace();
 
         if (mt1 == null) {
             return Optional.empty();
         }
         return Optional.of(new EstimatedRobotPose(mt1, this));
+    }
+
+    public void setCameraOffset(VisionConstants.CameraConfiguration cameraOffset){
+        LimelightHelpers.setCameraPose_RobotSpace(
+                getName(),
+                cameraOffset.getX(),
+                offset.getY(),
+                offset.getZ(),
+                offset.getRotation().getX(),
+                offset.getRotation().getY(),
+                offset.getRotation().getZ()
+        );
     }
 
     public record EstimatedRobotPose(LimelightHelpers.PoseEstimate poseEstimate, Limelight camera) {}
--- src/main/java/org/tahomarobotics/robot/vision/VisionConstants.java
+++ src/main/java/org/tahomarobotics/robot/vision/VisionConstants.java
@@ -1,6 +1,41 @@
 package org.tahomarobotics.robot.vision;
 
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Distance;
+import static edu.wpi.first.math.util.Units.degreesToRadians;
+import static edu.wpi.first.math.util.Units.inchesToMeters;
+
 public class VisionConstants {
+
+    public static CameraConfiguration Test_Camera;
+
+    static {
+        Test_Camera = new CameraConfiguration(
+                "Limelight",
+                new Transform3d(
+                        new Translation3d(inchesToMeters(1), inchesToMeters(1), inchesToMeters(1)),
+                        new Rotation3d(degreesToRadians(1), degreesToRadians(1), degreesToRadians(1))
+                        )
+                );
+    }
+
+    public record CameraConfiguration(String name, Transform3d offset){
+        public double[] getTransformArray() {
+            return new double[] {
+                    offset.getTranslation().getMeasureX().in(Units.Meters),
+                    offset.getTranslation().getMeasureY().in(Units.Meters),
+                    offset.getTranslation().getMeasureZ().in(Units.Meters),
+                    offset.getRotation().getMeasureX().in(Units.Degree),
+                    offset.getRotation().getMeasureY().in(Units.Degree),
+                    offset.getRotation().getMeasureZ().in(Units.Degree)
+            };
+        }
+
+    }
+
 
 
 
