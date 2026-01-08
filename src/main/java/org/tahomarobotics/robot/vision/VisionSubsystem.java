97e12989920a094533bb750b3f7a87e69b99addd
rodlewis360 <rodlewis360@gmail.com>
2026-01-08 12:40:56 -0800

    Switch to list of limelights in VisionSubsystem to allow for multiple cameras

--- src/main/java/org/tahomarobotics/robot/vision/VisionSubsystem.java
+++ src/main/java/org/tahomarobotics/robot/vision/VisionSubsystem.java
@@ -1,24 +1,30 @@
 package org.tahomarobotics.robot.vision;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.littletonrobotics.junction.Logger;
 import org.tahomarobotics.robot.util.AbstractSubsystem;
 
 public class VisionSubsystem extends AbstractSubsystem {
 
-    private final Limelight limelight;
+    private final List<Limelight> limelights;
 
     VisionSubsystem() {
         this(new Limelight(VisionConstants.TEST_CAMERA));
     }
 
-    @Override
-    public void subsystemPeriodic() {
-        limelight.getEstimatedRobotPose().ifPresent(
-            pose -> Logger.recordOutput("Vision/" + limelight.getName() + " Position", pose.poseEstimate().pose)
-        );
+    private VisionSubsystem(Limelight... limelights){
+        this.limelights = Arrays.asList(limelights);
     }
 
-    private VisionSubsystem(Limelight limelight){
-        this.limelight = limelight;
-    }
+    @Override
+    public void subsystemPeriodic() {
+        for (Limelight limelight : limelights) {
+            limelight.getEstimatedRobotPose().ifPresent(
+                pose -> Logger.recordOutput("Vision/" + limelight.getName() + " Position", pose.poseEstimate().pose)
+            );
+        }
+    }    
 }
